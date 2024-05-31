27933c85013cbcbc22896a42e09d40ae1ce0b515
CosmoGhost87 <120912513+CosmoGhost87@users.noreply.github.com>
2024-05-31 15:45:47 +0300

    +

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("genre_dataset.csv")
-games = np.sort(df["name"].unique())
-print(games)
+df["genre"] = df["genre"].str.split(",")
+temp = []
+for i in range (0, len(df)):
+    temp.append(set(df.loc[i,"genre"]))
+df["genre"] = temp
+print(df)
